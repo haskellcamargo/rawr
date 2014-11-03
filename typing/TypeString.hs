0deb399a6993bb2456d5d498373076502993a950
Marcelo Camargo <marcelocamargo@linuxmail.org>
2014-11-03 15:02:39 -0200

    Haskell TypeString
    Methods abstract definition to help in documentation

---
+++ typing/TypeString.hs
@@ -0,0 +1,23 @@
+module TypeString where
+
+-- Methods definition
+
+__construct :: String
+byteSize :: String -> Int
+capitalize :: String -> String
+chars :: String -> [String]
+chomp :: String -> [String]
+chr :: String -> Char
+clear :: String -> String
+codePoints :: String -> [Integer]
+concat :: String -> String
+downCase :: String -> String
+join :: String -> String -> [String]
+lines :: String -> [String]
+putStr :: String :: IO()
+putStrLn :: String :: IO()
+repeat :: String -> Integer -> String
+reverse :: String -> String
+tryConvert :: a -> Maybe String
+upCase :: String -> String
+words :: String -> [String]
