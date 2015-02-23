db33588ffdc9ac10081edf2be91d6b18e5b7b4d0
Marcelo Camargo <marcelocamargo@linuxmail.org>
2015-02-23 17:37:32 -0300

    Update Data.Tuple.php

--- Data.Tuple.php
+++ Data.Tuple.php
@@ -1,4 +1,10 @@
 <?php
+  # @author        => Marcelo Camargo
+  # @contributors  => []
+  # @creation_date => Unkown
+  # @last_changed  => 2015-02-23
+  # @package       => Data.Tuple
+
   # Copyright (c) 2014 Marcelo Camargo <marcelocamargo@linuxmail.org>
   #
   # Permission is hereby granted, free of charge, to any person
@@ -24,31 +30,66 @@
 
   require_once 'Data.Contract.ITuple.php';
 
+  # The ``Tuple`` type is a different way of storing multiple values in a single
+  # value. The main differences between tuples and lists are that tuples have a
+  # fixed number of elements (immutable); therefore, it makes sense to use
+  # tuples when you known in advance how many values are to be stored. For
+  # example, we might want a type for storing 2D coordinates of a point. We
+  # known exactly how many values we need for each point (two - the x and y
+  # coordinates), so tuples are applicable. The elements of a tuple do not need
+  # to be all of the same type. For instance, in a phonebook application we
+  # might want to handle the entries by crunching three values into one: the
+  # name, phone number, and the address of each person. In such a case, the
+  # three values won't havve the same type, so lists wouldn't help, but tuples
+  # would.
   class Tuple extends DataTypes implements Contract\ITuple {
+    # $size determines the size that a tuple have, statically. This value is
+    # immutable. $type represents a list containing the type of each element.
     private $size =  0
           , $type = [];
 
-    function __construct(Array $arr) {
-      $this->size = count($arr);
-      foreach ($arr as $node)
+    # Constructor is variadic. ITuples should be classified as dependent types.
+    # ONLY objects can be put inside tuples. This is made to avoid workarounds
+    # that the programmer can do.
+    function __construct() {
+      $this->size = func_num_args();
+      foreach ($args = func_get_args() as $item)
         $this->type[] = parent :: typeName(get_class($node));
-      $this->value = $arr;
+      $this->value = $args;
     }
 
-    function get(Num $index) {
-      return $this->value[$index - 1];
+    # Returns `Just` the first element of a tuple (if it exists), or `Nothing`.
+    function fst() { # :: Tuple<a>(Int) -> Maybe a
+      return isset($this->value[0])? Just($this->value[0])
+      /* otherwise */              : Nothing();
     }
 
-    function fst() {
-      return $this->value[0];
+    # Works like a 1-indexed array, where you get `Just` the element in the
+    # received index or ``Nothing``.
+    function get(Num $index) { # :: (Tuple, Int) -> Maybe a
+      return isset($this->value[$index - 1])? Just($this->value[$index - 1])
+      /* otherwise */                       : Nothing();
     }
 
-    function snd() {
-      return $this->value[1];
+    # Returns `Just` the second element of a tuple (if it exists), or `Nothing`.
+    function snd() { # :: Tuple<a, b>(Int) -> Maybe b
+      return isset($this->value[1])? Just($this->value[1])
+      /* otherwise */              : Nothing();
     }
 
-    function showType() {
+    # Returns the type of the items contained in the tuples in format of a
+    # `Data.Str`.
+    function showType() { # :: Tuple<a, b>(Int) -> Maybe b
       return new \Data\Str(
         "Tuple<" . implode(", ", $this->type) . ">({$this->size})");
+    }
+
+    # Swaps the values of a pair and returns `Just` the tuple or returns
+    # `Nothing` in case of not-a-pair.
+    function swap() { # :: Tuple -> Maybe Tuple
+      if ($this->size == 2)
+        return Just(Tuple($this->value[1]
+                        , $this->value[0]));
+      return Nothing();
     }
   }
