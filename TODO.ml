490090748be87cddfde12e9ad18ab88f18f6638c
haskellcamargo <marcelocamargo@linuxmail.org>
2015-02-24 23:28:23 -0300

    JSON for control

--- TODO.ml
+++ TODO.ml
@@ -1,10 +1,27 @@
-[READY:
-  [FILES:
-    [Data.Bool]
-    [Data.Contract.IBool]
-    [Data.Contract.IInt]
-    [Data.Contract.IMaybe]
-    [Data.Contract.ITuple]
-    [Data.Int]
-    [Data.Maybe]
-    [Data.Tuple]]
+progress: {
+  ready: {
+    types: [
+      "Data.Bool"
+    , "Data.Either"
+    , "Data.Either.Left"
+    , "Data.Either.Right"
+    , "Data.Maybe"
+    , "Data.Maybe.Just"
+    , "Data.Maybe.Nothing"
+    , "Data.Num"
+    , "Data.Num.Float"
+    , "Data.Num.Int"
+    , "Data.Tuple"
+    ]
+  , contracts: []
+  }
+  documented: {
+    types: [
+      "Data.Bool"
+    , "Data.Either"
+    , "Data.Maybe"
+    , "Data.Tuple"
+    ]
+  , contracts: []
+  }
+}
